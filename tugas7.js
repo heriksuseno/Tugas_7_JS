5e65e5b73c617887f3657673a58ffdb420cae40f
heriksuseno <>
2021-02-14 13:08:15 +0700

    Tugas 7 JS Untuk NIOMIC

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var tinggibadan = [165, 168, 171, 174, 177, 180, 183, 186, 189, 192];
+for (var i = 0; i < tinggibadan.length; i++) {
+  console.log(tinggibadan[i])
+}
+
+for (let x of tinggibadan) {
+  console.log('Dengan For Of -' + x);
+}
